113445413eefb41cd5acfd11a2d42526836e2fd3
Suraj Barnwal <surajbarnwalsuryavanshi>
2026-06-11 12:17:19 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './utils/db.js';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
+
 dotenv.config();
 const app = express();
 
@@ -15,8 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true, // Allow cookies to be sent
+  origin: "http://localhost:5173",
+  credentials: true, // Allow cookies to be sent
 };
 
 app.use(cors(corsOptions));
@@ -29,7 +31,18 @@ app.use("/api/company", companyRoute);
 app.use("/api/job", jobRoute);
 app.use("/api/application", applicationRoute);
 
+// -----------Code for Deployment------------------
+if (process.env.NODE_ENV === "production") {
+  const dirpath = path.resolve();
+
+  app.use(express.static(path.join(dirpath, "client/dist")));
+
+  app.use((req, res) => {
+    res.sendFile(path.join(dirpath, "client/dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
-    connectDB();
-    console.log(`Server is running on port ${PORT}`);
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
 });
